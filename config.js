c4cf28021f7d668c6d51cdd94ec7cd1a67ff4295
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-11-10 23:06:21 +0100

    cluster

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
     port: 3012,
+    useCluster: true,
 };
--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 #!/usr/bin/env node
 
-const cluster = require('cluster');
 const http = require('http');
-const numCPUs = require('os').cpus().length;
+const config = require('./config.js');
 
 let Application = require('./application/index.js');
 
-if (cluster.isMaster)
-{
-    // create workers
-    console.dir(`Cpu num: ${numCPUs}`);
-    for (let i = 0; i < numCPUs; i++)
-    {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died, replacing...`);
-        cluster.fork();
-    });
-}
-else
-{
+const launchApp = () => {
     // creating worker application, it will share all tcp connections
     // https://linuxtrainers.wordpress.com/2014/12/31/how-fork-system-call-works-what-is-shared-between-parent-and-child-process/
     (new Application()).launch();
+};
+
+if (config.useCluster)
+{
+    const numCPUs = require('os').cpus().length;
+    const cluster = require('cluster');
+
+    if (cluster.isMaster)
+    {
+        // create workers
+        console.dir(`Cpu num: ${numCPUs}`);
+        for (let i = 0; i < numCPUs; i++)
+        {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} died, replacing...`);
+            cluster.fork();
+        });
+    }
+    else
+    {
+        launchApp();
+    }
+}
+else
+{
+    launchApp();
 }
